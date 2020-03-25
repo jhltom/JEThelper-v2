e8c077d05fa8535cbcd93039410e36359f8dccca
Tom JH Lee <jhltom@seas.upenn.edu>
2020-03-25 00:25:41 -0400

    JEThelper page - added access permission stage

--- jet-helper-client/src/components/JEThelper.js
+++ jet-helper-client/src/components/JEThelper.js
@@ -8,7 +8,9 @@ export default class JEThelper extends React.Component {
     super(props);
 
     this.state = {
-      foundSubmissions: []
+      foundSubmissions: [],
+      accessGranted: false,
+      accessKey: ""
     }
   }
 
@@ -21,38 +23,36 @@ export default class JEThelper extends React.Component {
       }, err => {
         console.log(err);
       }).then(submissionList => {
-
-        let submissionDivs = submissionList.map((submission, i) =>{
-
-
-          console.log(Date.now())
-          console.log(submission.dateUnixTime);
-          const date = new Date(submission.dateUnixTime );
-          console.log(date);
-
-
-
-
-
+        let submissionDivs = submissionList.map((submission, i) => {
+          const date = new Date(submission.dateUnixTime);
           return (
             <div key={i} className="submission">
-            <div className="authorid">{submission.id}</div>
-            <div className="date">{date.toDateString()}</div>
-            <div className="title">{submission.title}</div>
-            <div className="reviewstatus">{submission.reviewStatus}</div>
-          </div>
-
-          )
-          
-        }
-          );
-
+              <div className="authorid">{submission.id}</div>
+              <div className="date">{date.toDateString()}</div>
+              <div className="title">{submission.title}</div>
+              <div className="reviewstatus">{submission.reviewStatus}</div>
+            </div>
+          )}
+        );
         this.setState({
           foundSubmissions: submissionDivs
         });
       }, err => {
         console.log(err);
       });
+  }
+
+  handleAccessKeyChange = (event) => {
+    this.setState({ accessKey: event.target.value });
+  }
+
+  submitAccessKey = () => {
+    if (this.state.accessKey === "testing"){
+      alert("Access Granted! ");
+      this.setState({accessGranted: true})
+    } else {
+      alert("Access Denied! Only AJLAS editors have access.");
+    }
   }
 
   render() {
@@ -62,22 +62,34 @@ export default class JEThelper extends React.Component {
         <div className="container jet-container">
           <div className="jumbotron less-headspace">
 
-            <div className="jet-header"> Submissions for Volume 33 Issue 2 : </div>
-            <br></br>
+            {this.state.accessGranted ? (
 
-            <div className="header-container">
-              <div className="headers">
-                <div className="header"><strong>Author Id </strong></div>
-                <div className="header"><strong>Submission Date</strong></div>
-                <div className="header"><strong>Title</strong></div>
-                <div className="header"><strong>Review Status</strong></div>
+              <div>
+                <div className="jet-header"> Submissions for Volume 33 Issue 2 : </div>
+                <br></br>
+                <div className="header-container">
+                  <div className="headers">
+                    <div className="header"><strong>Author Id </strong></div>
+                    <div className="header"><strong>Submission Date</strong></div>
+                    <div className="header"><strong>Title</strong></div>
+                    <div className="header"><strong>Review Status</strong></div>
+                  </div>
+                </div>
+                <div className="results-container" id="results">
+                  {this.state.foundSubmissions}
+                </div>
               </div>
-            </div>
-
-            <div className="results-container" id="results">
-              {this.state.foundSubmissions}
-            </div>
-
+            ) : (
+                <div>
+                  <div className="input-container">
+                    <input type='password' placeholder="Enter Access Key"
+                      value={this.state.accessKey}
+                      onChange={this.handleAccessKeyChange}
+                      className="access-input" />
+                    <button id="submitTitleBtn" onClick={this.submitAccessKey} className="submit-btn">Submit</button>
+                  </div>
+                </div>
+              )}
           </div>
         </div>
       </div>

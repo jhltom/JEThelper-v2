91f11e02c33caa16d99b564eda238ca178ff93f5
Tom JH Lee <jhltom@seas.upenn.edu>
2020-03-28 22:24:09 -0400

    JETsubmissionDetails added reviewers columns

--- jet-helper-client/src/components/JEThelper/JETsubmissionDetails.js
+++ jet-helper-client/src/components/JEThelper/JETsubmissionDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../style/JEThelper.css';
+import '../../style/JETsumissionDetails.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageNavbar from '../PageNavbar';
 
@@ -8,22 +8,49 @@ export default class JETsubmissionDetails extends React.Component {
     super(props);
 
     this.state = {
-      submission: []
+      submission: [],
+      reviewer1: false,
+      reviewer2: false,
+      reviewer3: false
     }
   }
 
   componentDidMount = () => {
-    // const { submissionId } = this.props.match.params
-    // console.log(submissionId)
     const { submission } = this.props.location.state
-    console.log(submission)
+
     const date = new Date(submission.dateUnixTime);
+
+    if (submission.reviewer1Id) {
+      this.setState({ reviewer1: true });
+    }
+    if (submission.reviewer2Id) {
+      this.setState({ reviewer2: true });
+    }
+    if (submission.reviewer3Id) {
+      this.setState({ reviewer3: true });
+    }
+
     let submissionDivs = [
       <div key={1} className="submission">
         <div className="authorid">{submission.authorId}</div>
         <div className="date">{date.toDateString()}</div>
         <div className="title">{submission.title}</div>
-        <div className="title">{submission.reviewStatus}</div>
+        {this.state.reviewer1 ?
+          <div className="reviewer1Id">{submission.reviewer1Id}</div>
+          :
+          <div className="reviewer1Id">-</div>
+        }
+        {this.state.reviewer2 ?
+          <div className="reviewer2Id">{submission.reviewer2Id}</div>
+          :
+          <div className="reviewer2Id">-</div>
+        }
+        {this.state.reviewer3 ?
+          <div className="reviewer3Id">{submission.reviewer3Id}</div>
+          :
+          <div className="reviewer3Id">-</div>
+        }
+        <div className="reviewstatus">{submission.reviewStatus}</div>
       </div>
     ];
     this.setState({
@@ -45,6 +72,9 @@ export default class JETsubmissionDetails extends React.Component {
                   <div className="header"><strong>Author Id </strong></div>
                   <div className="header"><strong>Submission Date</strong></div>
                   <div className="header"><strong>Title</strong></div>
+                  <div className="header"><strong>Reviewer1</strong></div>
+                  <div className="header"><strong>Reviewer2</strong></div>
+                  <div className="header"><strong>Reviewer3</strong></div>
                   <div className="header"><strong>Review Status</strong></div>
                 </div>
               </div>
